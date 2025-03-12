d59b7a0ac74de5d010dfb0b0bd1b9283b6c9ccf2
Daniil <garashkyan@bk.ru>
2025-03-12 13:14:37 +0300

    второй комит

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useState } from "react";
+
 
 export const App = () => {
+  const [value, setValue] = useState(10)
+  const increment = (value: any) => {
+   setValue (value => value +1 )
+  }
+  const dicrement = (value: any) => {
+    setValue (value => value - 1 )
+   }
   return (
     <div>
-      Hello world
+      Hello world{value}
+      <button onClick={increment}>increment</button>
+      <button onClick={dicrement}>increment</button>
+
     </div>
   )
 }
