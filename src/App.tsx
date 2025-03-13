0cbc3bd01bfd5d910ba7565d845138d922a6186c
Daniil <garashkyan@bk.ru>
2025-03-13 18:24:47 +0300

    тиретий коммит

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,10 @@ import { useState } from "react";
 
 
 export const App = () => {
-  const [value, setValue] = useState(10)
-  const increment = (value: any) => {
-   setValue (value => value +1 )
-  }
-  const dicrement = (value: any) => {
-    setValue (value => value - 1 )
-   }
+ 
   return (
     <div>
-      Hello world{value}
-      <button onClick={increment}>increment</button>
-      <button onClick={dicrement}>increment</button>
-
+      Hello world
     </div>
   )
 }
